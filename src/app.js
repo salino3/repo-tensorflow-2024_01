bc04c256257034ce30a7a4c38d56ff3fd4661897
Salino3 <flaviobcn012@gmail.com>
2024-12-09 12:50:17 +0100

    commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+console.log("1)", tf.tensor1d([1, 2, 3, 4]));
+
+console.log(
+  "2)",
+  tf.tensor(
+    [
+      [1.5, 2],
+      [3, 4],
+    ],
+    null,
+    "int32"
+  )
+);
+
+tf.tensor(
+  [
+    [1.5, 2],
+    [3, 4],
+  ],
+  null,
+  "int32"
+).print();
+
+tf.tensor([1, 2, 3, 4], [2, 2]).print();
+
+// same result
+console.log("3)", tf.scalar(3.14));
+console.log("4)", tf.tensor(3.14));
+tf.scalar(3.14).print();
+
+console.log("5)", tf.tensor1d([1, 2, 3]));
+
+const val = [];
+
+for (let i = 0; i < 30; i++) {
+  val[i] = Math.random(0, 100);
+}
+// 5: grupos, 3: filas, 2: columnas;
+tf.tensor3d(val, [5, 3, 2]).print();
---
+++ src/page2.js
@@ -0,0 +1,48 @@
+const val = [];
+
+for (let i = 0; i < 30; i++) {
+  val[i] = Math.random(0, 100) * 100;
+}
+// 5: grupos, 3: filas, 2: columnas;
+const tens = tf.tensor3d(val, [5, 3, 2], "int32");
+
+tens.print();
+
+// se queda en Promesa
+// console.log("1)", tens.data());
+
+//* estos 3 da el mismo resultado
+// tens.data().then(function (res) {
+//   console.log(res);
+// });
+
+// .data() es asincrono
+tens.data().then((res) => {
+  console.log("3)", res[2]);
+});
+
+// esto se ejecuta antes del .then(), es sincrono
+console.log("4)", tens.dataSync());
+
+console.log("//--------------------------------------------------------");
+
+const x = tf.variable(tf.tensor([1, 2, 3]));
+x.print();
+
+// se utiliza .assign() con 'const' y no 'let' para mantener la misma referencia del tensor
+// y no se podría realizar actualizaciones en el contexto adecuado, por ejemplo durante el entrenamiento de un modelos de entrenamiento y optimización.
+x.assign(tf.tensor([4, 5, 6]));
+
+x.print();
+
+const a = tf.tensor2d([
+  [1, 2, -3],
+  [4, 0, -2],
+]);
+const b = tf.tensor2d([
+  [3, 1],
+  [2, 4],
+  [-1, 5],
+]);
+tf.matMul(a, b).print();
+a.matMul(b).print();
